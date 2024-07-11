a89645f36bc37c4b01e0046c700e478a9da00a48
John Doran <netravelr@gmail.com>
2024-07-11 16:31:58 +0700

    Chapter 4 files

---
+++ Unity6_ShadersCookbook/Assets/Chapter 04/Scripts/SetRadiusProperties.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SetRadiusProperties : MonoBehaviour
+{
+    public Material radiusMaterial;
+    public float radius = 1;
+    public Color color = Color.white;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (radiusMaterial != null)
+        {
+            radiusMaterial.SetVector("_Center", transform.position);
+            radiusMaterial.SetFloat("_Radius", radius);
+            radiusMaterial.SetColor("_Radius_Color", color);
+        }
+
+    }
+}
